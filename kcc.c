ab9e029db1280a511ba1bc21faa5e20dd1fcd88a
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-03 21:00:06 +0900

    Add ignoring commenta // ... & /* ... */

--- kcc.c
+++ kcc.c
@@ -1,13 +1,99 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "kcc.h"
 
 
+static char* lex_emit(lex_state *state, uint64_t typ)
+{
+}
+
+static char* lex_skip_commnet(lex_state *state, char *src)
+{
+    while(1)
+    {
+        switch(*src)
+        {
+            case '*':
+                src++;
+                switch(src) {
+                    case '/':
+                        src++;
+                        return src;
+                    default:
+                        src++;
+                }
+            default:
+                continue;
+        }
+    }
+}
+
+static char* lex_skip_until(lex_state *state, char *src, char c)
+{
+    while(1)
+    {
+        if(*src == c)
+        {
+            return src;
+        }
+        src++;
+    }
+}
+
+
+static char* lex_pragma(state, char *pos)
+{
+    return pos;
+}
+
+char *lex_identify(lex_state *state, char* src)
+{
+    char *pos = src;
+    while(1)
+    {
+        switch(*pos)
+        {
+            case '/':
+                switch(pos)
+                {
+                    case '*':
+                        return lex_skip_commnet(state,  pos );
+                    case '/':
+                        return lex_skip_until(state, pos, '\n');
+                    default:
+                        goto fail;
+
+                }
+            case '#':
+                pos++;
+                return lex_pragma(state, pos);
+
+        }
+    }
+
+    return pos;
+
+fail:
+    return  NULL;
+}
 
 lex_state *lex(char *src)
 {
-    lex_state *tokens = NULL;
-    return tokens;
+    lex_state *state = (lex_state*)malloc( sizeof(lex_state) );
+    state->head = NULL;
+    state->tail = NULL;
+    char *c = src;
+    while(0)
+    {
+        c = lex_identify(state, c);
+        if(c == NULL)
+        {
+            break;
+        }
+    }
+
+    return state;
 }
 
 
