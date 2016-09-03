c26699a29318a7e8cb819668b8c8139e2a04c74c
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-03 19:39:30 +0900

    move lex utils

--- kcc.c
+++ kcc.c
@@ -1,23 +1,12 @@
 #include <stdint.h>
 #include <stdio.h>
+#include "kcc.h"
 
-struct lex_token_s {
-    uint64_t pos;
-    uint64_t len;
-    char *sym;
-    uint64_t typ;
-};
 
-typedef struct lex_token_s lex_token_t;
 
-struct lex_tokens_s {
-};
-
-typedef struct lex_token_s lex_tokens_t;
-
-lex_tokens_t *lex(char *src)
+lex_state *lex(char *src)
 {
-    lex_tokens_t *tokens = NULL;
+    lex_state *tokens = NULL;
     return tokens;
 }
 
--- kcc.h
+++ kcc.h
@@ -1,9 +1,51 @@
 #ifndef KCC_H
 #define KCC_H
+#include "stdint.h"
+
+struct lex_token_s;
+typedef struct lex_token_s lex_token_t;
 
 struct lex_tokens_s;
 typedef struct lex_tokens_s lex_tokens_t;
 
+struct lex_state_s;
+typedef struct lex_state_s lex_state;
+
+struct lex_token_s {
+    uint64_t pos;
+    uint64_t len;
+    char *sym;
+    uint64_t typ;
+};
+
+
+struct lex_tokens_s {
+    lex_token_t *token;
+    lex_tokens_t *next;
+};
+
+
+struct lex_state_s {
+    lex_tokens_t *head;
+    lex_token_t *data;
+    lex_tokens_t *tail;
+};
+
+
 void compile(const char *);
-lex_tokens_t *lex(char *);
+lex_state *lex(char *);
+
+#define LEX_TOKEN_BRACE_L 1
+#define LEX_TOKEN_BRACE_R 2
+#define LEX_TOKEN_BRACKET_L 3
+#define LEX_TOKEN_BRACKET_R 4
+#define LEX_TOKEN_SEMICORON 5
+#define LEX_TOKEN_RETURN 6
+#define LEX_TOKEN_DIGIT 7
+#define LEX_TOKEN_TYPE 8
+#define LEX_TOKEN_ID 9
+#define LEX_TOKEN_QUATE 10
+#define LEX_TOKEN_CORON 11
+#define LEX_TOKEN_EOL 12
+
 #endif
