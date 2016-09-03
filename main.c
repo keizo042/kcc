3b3bc8fb947e172a10734cf019162bed1fe31b2b
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-03 21:31:36 +0900

    Add usage

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "kcc.h"
+
+static char* text[] = {
+    "usage : kcc [file] ",
+    " ",
+    " ",
+    " ",
+    " ",
+    NULL
+};
+
+void usage()
+{
+    for(int i = 0; i <  7; i++)
+    {
+        puts(text[i]);
+    }
+}
 
 int
 main(int argc, char* argv[]) {
