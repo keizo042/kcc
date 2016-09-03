21d2ef82a6535f0a1d75c306ecd657ce21651ae3
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-03 19:55:29 +0900

    Update test

--- test/main.c
+++ test/main.c
@@ -6,32 +6,6 @@
 const char *src = "int func() {return 0; }";
 
 
-lex_token_t expect[] = {
-    {0,     3,  "int",      LEX_TOKEN_TYPE      },
-    {5,     4,  "func",     LEX_TOKEN_ID        },
-    {10,    1,  "(",        LEX_TOKEN_BRACKET_L },
-    {11,    1,  ")",        LEX_TOKEN_BRACKET_R },
-    {13,    1,  "{",        LEX_TOKEN_BRACE_R   },
-    {14,    6,  "return",   LEX_TOKEN_RETURN    },
-    {21,    1,  "0",        LEX_TOKEN_DIGIT     },
-    {22,    1,  ";",        LEX_TOKEN_SEMICORON },
-    {23,    1,  "}",        LEX_TOKEN_BRACE_R   },
-    {}
-};
-
-lex_tokens_t * build_expect_to_tokens(lex_token_t **exp)
-{
-    lex_tokens_t *tokens = (lex_tokens_t*)malloc( sizeof(lex_tokens_t));
-    lex_tokens_t *ret;
-    ret = tokens;
-    for(int i =0; exp[i] != NULL; i++)
-    {
-         tokens->token = &expect[i];
-         tokens->next = (lex_tokens_t*)malloc( sizeof(lex_tokens_t));
-         tokens = tokens->next;
-    }
-    return ret;
-}
 
 
 int lex_test1(lex_tokens_t *l);
@@ -65,8 +39,51 @@ fail:
     return -1;
 }
 
+static lex_token_t expect_test1[] = {
+    {0,     3,  "int",      LEX_TOKEN_TYPE      },
+    {5,     4,  "func",     LEX_TOKEN_ID        },
+    {10,    1,  "(",        LEX_TOKEN_BRACKET_L },
+    {11,    1,  ")",        LEX_TOKEN_BRACKET_R },
+    {13,    1,  "{",        LEX_TOKEN_BRACE_R   },
+    {14,    6,  "return",   LEX_TOKEN_RETURN    },
+    {21,    1,  "0",        LEX_TOKEN_DIGIT     },
+    {22,    1,  ";",        LEX_TOKEN_SEMICORON },
+    {23,    1,  "}",        LEX_TOKEN_BRACE_R   },
+    {0,     0,  "",         LEX_TOKEN_EOL       },
+    {}
+};
+
+static lex_tokens_t * build_expect_to_tokens(lex_token_t *exp)
+{
+    lex_tokens_t *tokens = (lex_tokens_t*)malloc( sizeof(lex_tokens_t));
+    lex_tokens_t *ret;
+    ret = tokens;
+    for(int i =0; i < sizeof(expect_test1) / sizeof(lex_tokens_t) ; i++)
+    {
+         tokens->token = &expect_test1[i];
+         tokens->next = (lex_tokens_t*)malloc( sizeof(lex_tokens_t));
+         tokens = tokens->next;
+    }
+    tokens->next = NULL;
+    return ret;
+}
+
+static int test1_token_free(lex_tokens_t *tokens)
+{
+    lex_tokens_t *next = NULL;
+    while(tokens->next != NULL)
+    {
+    next = tokens->next;
+    free(tokens);
+    tokens = next;
+    }
+    return 0;
+}
 
 int lex_test1(lex_tokens_t *l)
 {
-    return -1;
+    lex_tokens_t *tokens = build_expect_to_tokens(expect_test1);
+
+    test1_token_free(tokens);
+    return 0;
 }
