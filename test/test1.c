844c0dc509c358f5c2619e3b5e0c3599d716c796
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-05 20:37:55 +0900

    Add expressed definition

--- test/test1.c
+++ test/test1.c
@@ -1,37 +1,47 @@
 #include "test.h"
 
-char *test1_code_function = "int func() {return 0; }";
+
 static int lex_test1(lex_tokens_t *l);
 
+char *test1_code_function = "int func() {return 0; }";
+
 static lex_token_t test1_expect_tokens[] = {
-    { .pos = 0,     .len = 3,   .line = 0,  "int",      LEX_TOKEN_TYPE      },
-    { .pos = 5,     .len = 4,   .line = 0,  "func",     LEX_TOKEN_ID        },
-    { .pos = 10,    .len = 1,   .line = 0,  "(",        LEX_TOKEN_BRACKET_L },
-    { .pos = 11,    .len = 1,   .line = 0,  ")",        LEX_TOKEN_BRACKET_R },
-    { .pos = 13,    .len = 1,   .line = 0,  "{",        LEX_TOKEN_BRACE_R   },
-    { .pos = 14,    .len = 6,   .line = 0,  "return",   LEX_TOKEN_RETURN    },
-    { .pos = 21,    .len = 1,   .line = 0,  "0",        LEX_TOKEN_DIGIT     },
-    { .pos = 22,    .len = 1,   .line = 0,  ";",        LEX_TOKEN_SEMICORON },
-    { .pos = 23,    .len = 1,   .line = 0,  "}",        LEX_TOKEN_BRACE_R   },
-    { .pos = 0,     .len = 0,   .line = 0,  "",         LEX_TOKEN_EOL       },
+    { .pos = 0,     .len = 3,   .line = 0,  .sym = "int",       .typ = LEX_TOKEN_TYPE      },
+    { .pos = 5,     .len = 4,   .line = 0,  .sym = "func",      .typ = LEX_TOKEN_ID        },
+    { .pos = 10,    .len = 1,   .line = 0,  .sym = "(",         .typ = LEX_TOKEN_BRACKET_L },
+    { .pos = 11,    .len = 1,   .line = 0,  .sym = ")",         .typ = LEX_TOKEN_BRACKET_R },
+    { .pos = 13,    .len = 1,   .line = 0,  .sym = "{",         .typ = LEX_TOKEN_BRACE_L   },
+    { .pos = 14,    .len = 6,   .line = 0,  .sym = "return",    .typ = LEX_TOKEN_RETURN    },
+    { .pos = 21,    .len = 1,   .line = 0,  .sym = "0",         .typ = LEX_TOKEN_DIGIT     },
+    { .pos = 22,    .len = 1,   .line = 0,  .sym = ";",         .typ = LEX_TOKEN_SEMICORON },
+    { .pos = 24,    .len = 1,   .line = 0,  .sym = "}",         .typ = LEX_TOKEN_BRACE_R   },
+    { .pos = 25,     .len = 0,   .line = 0,  .sym = "",          .typ = LEX_TOKEN_EOL       },
     {}
 };
 
 int lex_test1(lex_tokens_t *tokens)
 {
     lex_tokens_t *expect = build_expect_tokens(test1_expect_tokens);
-    if(tokens == NULL)
+
+    lex_tokens_t *e = expect,
+                 *t = tokens;
+    while(1)
     {
-        expect_tokens_free(expect);
-        return -1;
-    }
-    while(0)
-    {
-        if(tokens == NULL)
+        if(t == NULL && e == NULL)
+        {
+            break;
+        }
+        if(strncmp(e->token->sym, t->token->sym, strlen(e->token->sym)) != 0)
+        {
+            return -1;
+        }
+
+        if(e->token->typ != t->token->typ)
         {
             return -2;
-        
         }
+        e = e->next;
+        t = t->next;
     }
 
     expect_tokens_free(expect);
