24023395dab2bd888e8fde33ab8207250ce13fef
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-06 00:35:25 +0900

    Update test

--- test/test_lex_digit.c
+++ test/test_lex_digit.c
@@ -7,26 +7,26 @@ int
 main(int argc, char* argv[]) {
 
     char *test1 = "1234567";
-    if(test_lex_digit(test1) != 0)
+    if(test_lex_digit(test1) != CONTINUE)
     {
         ERROR("test_lex_digit", "test1", "fail");
     }
 
 
     char *test2 = "0000";
-    if(test_lex_digit(test2) == 0)
+    if(test_lex_digit(test2) == CONTINUE)
     {
         ERROR("test_lex_digit", "test2", "fail");
     }
 
     char *test3 = "0.0";
-    if(test_lex_digit(test3) != 0)
+    if(test_lex_digit(test3) != CONTINUE)
     {
         ERROR("test_lex_digit", "test3", "fail");
     }
 
     char *test4 = "hello world";
-    if(test_lex_digit(test4) == 0)
+    if(test_lex_digit(test4) == ERR)
     {
         ERROR("test_lex_digit", "test4", "fail");
     }
