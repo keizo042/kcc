7549584136e3ea156a7f9b959fe647f39132d6a3
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-07 23:30:19 +0900

    Update test for lex_digit

--- test/test_lex_digit.c
+++ test/test_lex_digit.c
@@ -11,11 +11,17 @@ main(int argc, char* argv[]) {
     char *test2 = "0000";
     char *test3 = "0.0";
     char *test4 = "hello world";
+    char *test5 = "1e1";
+    char *test6 = "1e100";
+    char *test7 = "0.02e100";
 
     TEST(test_lex_digit(test1) != CONTINUE, F, "test1", "fail");
     TEST(test_lex_digit(test2) != CONTINUE, F, "test2", "fail");
     TEST(test_lex_digit(test3) != CONTINUE, F, "test3", "fail");
     TEST(test_lex_digit(test4) != ERR, F, "test4", "fail");
-    
+    TEST(test_lex_digit(test5) != CONTINUE, F, "test5", "fail");
+    TEST(test_lex_digit(test6) != CONTINUE, F, "test6", "fail");
+    TEST(test_lex_digit(test7) != CONTINUE, F, "test7", "fail");
+
     return 0;
 }
