ff02ba7b4d2892d47356a33db05051c92603e2e0
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-13 17:31:20 +0900

    WIP option parser

--- main.c
+++ main.c
@@ -12,6 +12,16 @@ static char* text[] = {
     NULL
 };
 
+typedef struct field_s {
+    char *opt;
+    char *value;
+}field_t;
+
+typedef struct optargs_s {
+    field_t** filed;
+    uint64_t mode;
+}optargs_t;
+
 void usage()
 {
     for(int i = 0; i <  7; i++)
@@ -20,18 +30,16 @@ void usage()
     }
 }
 
-void optparse()
+optargs_t* optparse(int argc, char**argv)
 {
+    return NULL;
 }
 
 int
 main(int argc, char* argv[]) {
 
-    if( argc < 2)
-    {
-        usage();
-        return 0;
-    }
+    optargs_t* ARGS = optparse(argc, argv);
+
     
     return 0;
 }
