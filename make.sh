956dafa2fb38a47998943eed0d705bbae7df6c72
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-25 15:15:49 +0900

    UPDATE make file

--- make.sh
+++ make.sh
@@ -1,40 +1,45 @@
 #!/usr/bin/env bash
 PWD=`dirname $0`
 INCLUDE_DIR=""
-FILES="kcc lex parse ast"
-INCLUDE_DIR=""
+INCLUDE_DIR="$PWD"
+TEST_DIR="$PWD/test"
 TEST_FILES=" "
-TEST_INCLUDE_DIR="$PWD/test"
+TEST_INCLUDE_DIR=$TEST_DIR
 CC="clang -std=c99 -std=gnu99"
 
 case $1 in
     "test" )
-        for f in $FILES 
+        for f in $(ls $PWD | grep .c$)
         do
-            $CC -g -c "$f.c"
+            $CC -g -I$INCLUDE_DIR -c $f
             if [ ! $? -eq 0 ]; then
                 echo "fail"
                 exit -1;
             fi
         done
+
+        for f in $( ls $TEST_DIR | grep .c$ )
+        do
+            $CC -g -I$INCLUDE_DIR -I$TEST_INCLUDE_DIR "$TEST_DIR/$f"  -o $TEST_DIR/${f%.c}.out
+        done
         ;;
     "clean")
-        rm *.o  ./kcc >> /dev/null 2>&1
+        rm -rf *.o  ./kcc $TEST_DIR/*.out $TEST_DIR/*.dSYM >> /dev/null 2>&1
 
         ;;
     * )
         F=""
-        for f in $FILES 
+        for f in $(ls $PWD | grep .c$ | grep -v main.c )
         do
-            $CC -c "$f.c"
+            $CC -c -I$INCLUDE_DIR $f
             if [ ! $? -eq 0 ]; then
                 echo "fail"
                 exit -1;
             fi
-            F="$F $f.o"
+            F="$F ${f%.c}.o"
         done
-        $CC -c main.c
+        $CC -I"$INCLUDE_DIR"  -c main.c 
         echo "build kcc..."
-        $CC -I"$INCLUDE_DIR" $F main.o -o kcc 
+        $CC -I"$INCLUDE_DIR"  main.o -o kcc 
         ;;
 esac
