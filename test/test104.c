4fbcf075053726e4b1987ae09e6c962a17866eae
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-27 22:58:59 +0900

    Add comment test

--- test/test104.c
+++ test/test104.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include "lex.h"
+#include "test.h"
+
 
 int
 main(int argc, char* argv[]) {
+    char *test1 = "/* */";
+    char *test2 = "//     ";
+    char *test3 = "/*    ";
+    char *test4 = "//  /* */";
+    char *test5 = "/*  //  */";
+    
+    test(test400(test1) == LEX_FIN, __FILE__, "comment1");
+    test(test400(test2) == LEX_FIN, __FILE__, "comment2");
+    test(test400(test3) == LEX_ERR, __FILE__, "comment3");
+    test(test400(test4) == LEX_FIN, __FILE__, "comment4");
+    test(test400(test5) == LEX_FIN, __FILE__, "comment5");
     
     return 0;
 }
