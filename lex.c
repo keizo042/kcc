9e6a4bd6a912a074cfcb18e1a34fc957760e6de2
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-02-21 00:47:03 +0900

    UPDATE

--- lex.c
+++ lex.c
@@ -4,8 +4,8 @@
 #include <string.h>
 
 #define LEX_OK 0
-#define LEX_CONT 1
-#define LEX_ERR 2
+#define LEX_CONTINUE 1
+#define LEX_ERROR 2
 
 lex_state *lex_init(char *src) {
     lex_state *state = (lex_state *)malloc(sizeof(lex_state));
@@ -16,14 +16,86 @@ int lex_state_run(lex_state *state) { return 0; }
 
 int lex_state_error(lex_state *state) { return state->err; }
 
-int lex_emit(lex_state *state, typ_t typ) {}
+int lex_state_eof(lex_state *state){return 0;}
 
-int lex_text(lex_state *state) {}
+int lex_emit(lex_state *state, typ_t typ) {
+    return LEX_CONTINUE;
+}
 
-int lex_digit(lex_state *state) {}
+char lex_state_pos_incr(lex_state *state){}
+char lex_state_pos_decr(lex_state *state){}
 
-int lex_ident(lex_state *state) {}
+char lex_state_incr(lex_state *state) {}
+
+char lex_state_decr(lex_state *state) {}
+
+
+char lex_state_peek(lex_state *state){
+    return state->src[state->pos+state->len];
+}
+
+int lex_digit(lex_state *);
+int lex_ident(lex_state *);
+
+int lex_text(lex_state *state) {
+    switch(lex_state_peek(state)) {
+        case '\n':
+            state->line++;
+        case ' ':
+        case '\t':
+            lex_state_pos_incr(state);
+            return LEX_CONTINUE;
+        case '"':
+            return lex_digit(state);
+        default:
+            return lex_ident(state);
+    }
+    return LEX_ERROR;
+}
+
+#define ISDIGIT(x) x
+int lex_digit(lex_state *state) {
+    char c;
+    while(1){
+        c = lex_state_peek(state);
+        if(lex_state_eof(state)) {
+            return LEX_ERROR;
+        }
+        if(ISDIGIT(c)){
+            lex_state_incr(state);
+            continue;
+        }else{
+            lex_state_decr(state);
+            break;
+        }
+
+    }
+    return LEX_CONTINUE;
+}
+
+#define ISIDENT(x) x
+int lex_ident(lex_state *state) {
+    char c;
+    while(1){
+        c = lex_state_peek(state);
+        if(lex_state_eof(state)){
+            return LEX_ERROR;
+        }
+        if(ISIDENT(c) ){
+            lex_state_incr(state);
+            continue;
+        }else{
+            break;
+        }
+
+    }
+    return LEX_CONTINUE;
+}
 
 
 
-int lex_string(lex_state *state) { return }
+int lex_string(lex_state *state) { 
+    switch (lex_state_peek(state)) {
+    }
+    return LEX_ERROR;
+}
--- lex.h
+++ lex.h
@@ -13,6 +13,14 @@
 #define LEX_TOKEN_DIV
 #define LEX_TOKEN_MOD
 
+typedef int lex_typ_t;
+
+struct lex_token_t;
+typedef struct lex_token_t lex_token_t;
+
+struct lex_token_list_t;
+typedef struct lex_token_list_t lex_token_list_t;
+
 struct lex_state;
 typedef struct lex_state lex_state;
 
@@ -23,8 +31,26 @@ typedef struct lex_state {
     int len;
     int line;
     int err;
+    
+    lex_token_list_t *start;
+    lex_token_list_t *tail;
 
 } lex_state;
+
+typedef struct lex_token_t {
+        char *sym;
+        lex_typ_t typ;
+
+        int pos;
+        int len;
+        int line;
+}lex_token_t;
+
+typedef struct lex_token_list_t {
+        lex_token_list_t *prev;
+        lex_token_t *token;
+        lex_token_list_t *next;
+}lex_token_list_t;
 
 lex_state *lex_init(char *src);
 int lex_state_run(lex_state *);
