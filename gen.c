342e848ebebbecf972a0c82dca9a0f4083d74acb
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-02-22 20:45:46 +0900

    gen

---
+++ gen.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "ast.h"
+#include "parser.h"
+#include "gen.h"
+
+gen_info_t *gen_info_init(parser_state *state){
+    gen_info_t *info = (gen_info_t*)malloc( sizeof(gen_info_t) );
+    return info;
+}
+
+int gen_info_run(gen_info_t* info){
+}
+
+int gen_info_error(gen_info_t *info){
+    return 0;
+}
--- gen.h
+++ gen.h
@@ -1,9 +1,15 @@
 #ifndef GEN_H
 #define GEN_H
 #include "parser.h"
+
 struct gen_info_t;
 typedef struct gen_info_t gen_info_t;
+
+typedef struct gen_info_t {
+}gen_info_t;
+
 gen_info_t *gen_info_init(parser_state *);
-int gen_info_run();
-int gen_info_error();
+
+int gen_info_run(gen_info_t*);
+int gen_info_error(gen_info_t *);
 #endif
