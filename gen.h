3830e43248a1f00b37d9e639d01aae15eecdca6f
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-02-22 21:09:02 +0900

    update

--- gen.h
+++ gen.h
@@ -11,7 +11,7 @@ typedef struct gen_info_t {
 gen_info_t *gen_info_init(parser_state *);
 
 int gen_info_run(gen_info_t*);
-int gen_info_error(gen_info_t *);
+const char* gen_info_error(gen_info_t *);
 
 int gen_info_print(gen_info_t*);
 #endif
