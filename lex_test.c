18759fc240700c9471da9286452c621e0078ef61
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-06-22 03:17:51 +0900

    Add digit test

--- lex_test.c
+++ lex_test.c
@@ -1,7 +1,35 @@
 #include "lex.h"
 
+static int testing(char *code) {
+    lex_state *lexer = lex_state_init(code);
+    int rc           = lex_state_run(lexer);
+    if (rc) {
+        fprintf(stderr, "lex:%s\n", lex_state_error(lexer));
+        return 1;
+    }
+}
 
-int main(){
-    printf("not yet implemented\n");
-    return 1;
+
+int test1() { return testing("1 + 2"); }
+
+int test2() { return testing("1+2"); }
+
+int test3() { return testing("\"\""); }
+
+int test4() { return testing("\"hello world\""); }
+
+int test5() { return testing(""); }
+
+typedef int (*testfn_t)();
+
+testfn_t testcase[] = {test1, test2, NULL}; // test3, test4, test5, NULL};
+
+int main() {
+    for (int i = 0; testcase[i] != NULL; i++) {
+        testfn_t testfn = testcase[i];
+        if (testfn()) {
+            return 1;
+        }
+    }
+    return 0;
 }
