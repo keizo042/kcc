7f335a03c946817c69ef742a1ba682c192c7d4c4
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-06-22 03:24:32 +0900

    add test format

--- gen_test.c
+++ gen_test.c
@@ -5,9 +5,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-gen_info_t expect = {};
+
+int testing(parser_state *s) {
+    gen_info_t *gen = gen_info_init(s);
+    if (gen_info_run(gen)) {
+        return 1;
+    }
+    return 0;
+}
+
+int test1() { return 1; }
+
+typedef int (*testfn_t)();
+
+testfn_t testcase[] = {test1, NULL};
 
 int main() {
-    printf("not yet implemented\n");
+    for (int i = 0; testcase[i] != NULL; i++) {
+        testfn_t testfn = testcase[i];
+        if(testfn()){
+            return 1;
+        }
+    }
     return 1;
 }
