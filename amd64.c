e035532904f6ac13b58af99e18cf90c7e597fab4
Kouichi Nakanishi <keizo.bookman@gmail.com>
2017-06-22 02:24:36 +0900

    Remove

--- amd64.c
+++
@@ -1,6 +0,0 @@
-#include "amd64.h"
-#include "gen.h"
-
-int arch_amd64_compile(gen_info_t *info) {return 1;}
-
-const char *arch_amd64_error(gen_info_t *info) {return "";}
--- amd64.h
+++
@@ -1,8 +0,0 @@
-#ifndef ARCH_AMD_H
-#define ARCH_AMD_H
-#include "gen.h"
-
-int arch_amd64_compile(gen_info_t *info);
-
-const char *arch_amd64_error(gen_info_t *info);
-#endif
